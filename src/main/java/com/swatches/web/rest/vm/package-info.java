bec2bfd0674c1e368ea38aa724d4248b9aa224b4
Bryan <bryan@Zipcoders-MacBook-Pro-10.local>
2024-08-19 14:48:28 -0400

    Initial version of swatches generated by generator-jhipster@8.6.0

---
+++ src/main/java/com/swatches/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer visual models.
+ */
+package com.swatches.web.rest.vm;
---
+++ src/test/java/com/swatches/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.swatches.security.jwt;
+
+import com.swatches.config.SecurityConfiguration;
+import com.swatches.config.SecurityJwtConfiguration;
+import com.swatches.config.WebConfigurer;
+import com.swatches.management.SecurityMetersService;
+import com.swatches.web.rest.AuthenticateController;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    properties = {
+        "jhipster.security.authentication.jwt.base64-secret=fd54a45s65fds737b9aafcb3412e07ed99b267f33413274720ddbb7f6c5e64e9f14075f2d7ed041592f0b7657baf8",
+        "jhipster.security.authentication.jwt.token-validity-in-seconds=60000",
+    },
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        AuthenticateController.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+public @interface AuthenticationIntegrationTest {
+}
